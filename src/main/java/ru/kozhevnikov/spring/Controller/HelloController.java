b3dc8d6a7f44310793547f23e6383e61911a24f2
kozhevnikov <kozhevnikov.alex-2000@mail.ru>
2022-08-02 14:50:42 +0300

    refactor: renaming

--- src/main/java/ru/kozhevnikov/spring/Controller/HelloController.java
+++ src/main/java/ru/kozhevnikov/spring/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package ru.kozhevnikov.spring.controller;
+package ru.kozhevnikov.spring.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
